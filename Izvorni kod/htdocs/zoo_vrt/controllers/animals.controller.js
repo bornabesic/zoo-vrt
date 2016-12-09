d45701e1c7f7f0e8ebc68dfd72a8bb9e9cc5c9c1
Borna Bešić <borna.besic@fer.hr>
2016-12-09 03:32:29 +0100

    Osposobljen popup kod posvajanja. 'animals.html' cijeli uređen. U 'zoo_vrt.html' dodan BootstrapJS i BootstrapDialog (https://github.com/nakupanda/bootstrap3-dialog).

--- Izvorni kod/htdocs/zoo_vrt/controllers/animals.controller.js
+++ Izvorni kod/htdocs/zoo_vrt/controllers/animals.controller.js
@@ -1,9 +1,12 @@
 app.controller("AnimalsController", function($scope, $stateParams, AnimalsService){
 
+	//DIJALOG
+
+	//
 	$scope.animals = [];
 	var species_id = $stateParams["species_id"] //dohvati id iz URL-a
 
-	//fnction
+	//functions
 	function getAnimals(species_id){
 
 		AnimalsService.getAnimals(species_id).then(function(result) {
@@ -11,8 +14,36 @@ app.controller("AnimalsController", function($scope, $stateParams, AnimalsServic
 			$scope.$apply();
 		})
 	}
-	
 
+	function adoptAnimal(animal){
+		alert("Adopted "+animal.name+"!")
+	}
+
+	$scope.toggleFacts = function(animal){
+		var facts = $("#facts_"+animal.animal_id);
+		if(!facts.is(":visible")) facts.show()
+		else facts.hide()
+	}
+	
+	$scope.adoptionDialog = function(animal){
+		BootstrapDialog.show({
+		title: animal.name,
+	    message: 'Odabirom opcije \'Potvrdi\' postajete posvojitelj ove jedinke. Informacije o uplati će biti dostavljene na Vašu adresu e-pošte.',
+	    buttons: [{
+	   		label: 'Potvrdi',
+	        action: function(dialog) {
+	        	adoptAnimal(animal)
+	        	dialog.close()
+	        }
+	    },
+	    {
+	   		label: 'Zatvori',
+	        action: function(dialog) {
+	        	dialog.close()
+	        }
+	    }]
+		});
+	}
 
 	//init
 	getAnimals(species_id);
